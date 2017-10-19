4c161e26089a8eee05937e8098e95833511633d3
et-nik <nikita.hldm@gmail.com>
2017-10-19 19:29:43 +0300

    Changes

--- resources/assets/js/components/input-text-list.vue
+++ resources/assets/js/components/input-text-list.vue
@@ -1,7 +1,6 @@
 <template>
     <div>
         <a href="#" v-on:click="addItem">+</a>
-        <a href="#" v-on:click="printItems">print</a>
         <div class="form-group" v-for="(item, index) in items">
             <input v-bind:name="name + '[]'" type="text" v-bind:value="item" v-bind:id="name + '_' + index" class="form-control">
             <a href="#" v-on:click="removeItem(index)">X</a>
@@ -21,9 +20,6 @@
             },
             addItem: function() {
                 this.items.push('');
-            },
-            printItems: function() {
-                console.log(this.items);
             }
         }
     }
