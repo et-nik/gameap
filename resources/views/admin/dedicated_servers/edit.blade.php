a41fdef87aad8ae493e68620f6fe6162201e240a
ET-Nik <nikita.hldm@gmail.com>
2018-08-22 16:35:19 +0300

    Update styles

--- resources/views/admin/dedicated_servers/edit.blade.php
+++ resources/views/admin/dedicated_servers/edit.blade.php
@@ -16,45 +16,57 @@
     {!! Form::model($dedicatedServer, ['method' => 'PATCH', 'route' => ['admin.dedicated_servers.update', $dedicatedServer->id]]) !!}
     <div class="row">
         <div class="col-md-6">
-            {{ Form::bsText('name') }}
-            {{ Form::bsText('enabled') }}
-            {{ Form::bsText('os') }}
-            {{ Form::bsText('location') }}
-            {{ Form::bsText('provider') }}
-            
-            {{ Form::bsText('ram') }}
-            {{ Form::bsText('cpu') }}
-            {{ Form::bsText('work_path') }}
-            {{ Form::bsText('steamcmd_path') }}
+            <div class="card bg-light mb-3">
+                <div class="card-body">
+                    {{ Form::bsText('name') }}
+                    {{ Form::bsText('enabled') }}
+                    {{ Form::bsText('os') }}
+                    {{ Form::bsText('location') }}
+                    {{ Form::bsText('provider') }}
+
+                    {{ Form::bsText('ram') }}
+                    {{ Form::bsText('cpu') }}
+                    {{ Form::bsText('work_path') }}
+                    {{ Form::bsText('steamcmd_path') }}
+                </div>
+            </div>
         </div>
     
         <div class="col-md-6">
-            <div class="panel panel-default">
-                <div class="panel-heading">
+            <div class="card bg-light mb-3">
+                <div class="card-header">
                     IP List
                 </div>
-                <div class="panel-body">
+                <div class="card-body">
                     <input-text-list :initial-items="{{ json_encode($dedicatedServer->ip) }}" name="ip" label="IP"></input-text-list>
                 </div>
             </div>
         </div>
     
         <div class="col-md-6 col-md-offset-2">
-            {{ Form::bsText('script_start') }}
-            {{ Form::bsText('script_stop') }}
-            {{ Form::bsText('script_restart') }}
-            {{ Form::bsText('script_status') }}
-            {{ Form::bsText('script_get_console') }}
-            {{ Form::bsText('script_send_command') }}
+            <div class="card bg-light mb-3">
+                <div class="card-body">
+                    {{ Form::bsText('script_start') }}
+                    {{ Form::bsText('script_stop') }}
+                    {{ Form::bsText('script_restart') }}
+                    {{ Form::bsText('script_status') }}
+                    {{ Form::bsText('script_get_console') }}
+                    {{ Form::bsText('script_send_command') }}
+                </div>
+            </div>
         </div>
     
         <div class="col-md-12">
-            {{ Form::bsText('gdaemon_host') }}
-            {{ Form::bsText('gdaemon_login') }}
-            {{ Form::bsText('gdaemon_password') }}
-            {{ Form::bsText('gdaemon_privkey') }}
-            {{ Form::bsText('gdaemon_pubkey') }}
-            {{ Form::bsText('gdaemon_keypass') }}
+            <div class="card bg-light mb-3">
+                <div class="card-body">
+                    {{ Form::bsText('gdaemon_host') }}
+                    {{ Form::bsText('gdaemon_login') }}
+                    {{ Form::bsText('gdaemon_password') }}
+                    {{ Form::bsText('gdaemon_privkey') }}
+                    {{ Form::bsText('gdaemon_pubkey') }}
+                    {{ Form::bsText('gdaemon_keypass') }}
+                </div>
+            </div>
         </div>
     
     
