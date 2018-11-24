06736906a2f4841692b590d1ec9264371821dd7d
et-nik <nikita.hldm@gmail.com>
2018-11-24 16:29:54 +0300

    Add shortcodes descriptions

--- resources/views/admin/dedicated_servers/edit.blade.php
+++ resources/views/admin/dedicated_servers/edit.blade.php
@@ -87,17 +87,32 @@
 
                     </div>
                     <div class="card-footer">
-                        <h5>Game Server Shortcodes</h5>
-                        <ul>
-                            <li>{host} — server host/ip</li>
-                            <li>{port} — server port</li>
-                            <li>{query_port} — server query port</li>
-                            <li>{rcon_port} — server rcon port</li>
-                            <li>{dir} — absolute path to server directory</li>
-                            <li>{screen_name} — screen name</li>
-                            <li>{game} — game code</li>
-                            <li>{user} — user name (exist user in dedicated server, not admin panel)</li>
-                        </ul>
+                        <div class="row">
+                            <div class="col-6">
+                                <h5>Game Server Shortcodes</h5>
+                                <ul>
+                                    <li>{host} — server host/ip</li>
+                                    <li>{port} — server port</li>
+                                    <li>{query_port} — server query port</li>
+                                    <li>{rcon_port} — server rcon port</li>
+                                    <li>{dir} — absolute path to server directory</li>
+                                    <li>{screen_name} — screen name</li>
+                                    <li>{game} — game code</li>
+                                    <li>{user} — user name (exist user in dedicated server, not admin panel)</li>
+                                </ul>
+                            </div>
+                            <div class="col-6">
+                                <h5>Start/Restart Shortcodes</h5>
+                                <ul>
+                                    <li>{command} — Game server start command</li>
+                                </ul>
+
+                                <h5>Script Send Command</h5>
+                                <ul>
+                                    <li>{command} — Console command</li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
