e43632525377983ff810036cb4428786460fddeb
et-nik <nikita.hldm@gmail.com>
2018-11-30 19:10:57 +0300

    Cast empty attribute

--- app/Models/GameMod.php
+++ app/Models/GameMod.php
@@ -83,4 +83,20 @@ class GameMod extends Model implements ValidableContract
     {
         return $this->hasMany(Server::class);
     }
+
+    /**
+     * Cast an attribute to a native PHP type.
+     *
+     * @param  string  $key
+     * @param  mixed  $value
+     * @return mixed
+     */
+    protected function castAttribute($key, $value)
+    {
+        if ($this->getCastType($key) == 'array' && is_null($value)) {
+            return [];
+        }
+
+        return parent::castAttribute($key, $value);
+    }
 }
