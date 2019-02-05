d0248a38553db24279d3f04a90b60cadd3f0d3f3
et-nik <nikita.hldm@gmail.com>
2019-02-05 19:48:46 +0300

    Implement labels localization

--- resources/views/components/form/textarea.blade.php
+++ resources/views/components/form/textarea.blade.php
@@ -1,3 +1,11 @@
+@if (empty($label))
+    @php ($localeLabel = __('labels.' . $name))
+
+    @if (!empty($localeLabel) && $localeLabel != 'labels.' . $name)
+        @php ($label = $localeLabel)
+    @endif
+@endif
+
 <div class="form-group{{ $errors->has($name) ? ' has-error' : '' }}">
     {{ Form::label($name, $label, ['class' => 'control-label']) }}
     {{ Form::textarea($name, $value, array_merge(['class' => 'form-control'], $attributes)) }}
