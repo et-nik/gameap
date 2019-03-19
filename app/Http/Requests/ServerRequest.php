0fe09facc64888275e1c1cbf6a5943610b6747a7
et-nik <nikita.hldm@gmail.com>
2019-03-19 13:03:23 +0300

    Add gamea_mod_id validation rules

--- app/Http/Requests/ServerRequest.php
+++ app/Http/Requests/ServerRequest.php
@@ -10,7 +10,7 @@ class ServerRequest extends Request
             'name' => 'required|max:128',
             'game_id' => 'required',
             'ds_id' => 'required',
-            'game_mod_id' => '',
+            'game_mod_id' => 'required|exists:game_mods,id',
             'server_ip' => 'required',
             'server_port' => 'required|numeric|digits_between:1,65535',
             'dir' => 'required',
