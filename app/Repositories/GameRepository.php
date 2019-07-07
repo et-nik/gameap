e2513d60360532e1ec4dea27288f7b2d32dfba19
et-nik <nikita.hldm@gmail.com>
2019-07-07 19:43:32 +0300

    Fix game list upgrading

--- app/Repositories/GameRepository.php
+++ app/Repositories/GameRepository.php
@@ -6,6 +6,7 @@ use Gameap\Models\Game;
 use Gameap\Models\GameMod;
 use Gameap\Services\GlobalApi;
 use Symfony\Component\Finder\Glob;
+use Illuminate\Support\Arr;
 
 class GameRepository
 {
@@ -33,14 +34,24 @@ class GameRepository
         $apiGames = GlobalApi::games();
 
         foreach ($apiGames as $gameData) {
-            $game = Game::firstOrCreate(['code' => $gameData['code']]);
+            $game = Game::firstOrCreate([
+                'code' => $gameData['code'],
+                'start_code' => $gameData['start_code'],
+                'name' => $gameData['name'],
+                'engine' => $gameData['engine'],
+            ]);
 
             $game->fill($gameData);
             $game->save();
 
             if (!empty($gameData['mods'])) {
                 foreach ($gameData['mods'] as $gameModData) {
-                    $gameMod = GameMod::firstOrCreate(['name' => $gameModData['name']]);
+
+                    $gameMod = GameMod::firstOrCreate([
+                        'name' => $gameModData['name'],
+                        'game_code' => $gameData['code']
+                    ]);
+
                     $gameMod->fill($gameModData);
 
                     if ($gameMod->isValid()) {
