ffe1df88b8dc351ef450bb30ec93f7464dff4932
et-nik <nikita.hldm@gmail.com>
2019-07-10 18:00:03 +0300

    Move admin request Optimize getting games and game mods Test Admin/ServerController

---
+++ tests/Unit/Controllers/Admin/ServersControllerTest.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Tests\Unit\Admin;
+
+use Gameap\Http\Requests\Admin\ServerUpdateRequest;
+use Gameap\Models\Server;
+use Illuminate\Http\Response;
+use Tests\TestCase;
+use Gameap\Http\Controllers\Admin\ServersController;
+use Gameap\Http\Requests\Admin\ServerCreateRequest;
+use Illuminate\Container\Container;
+
+/**
+ * @covers \Gameap\Http\Controllers\Admin\ServersController<extended>
+ */
+class ServersControllerTest extends TestCase
+{
+    /**
+     * @var ServersController
+     */
+    protected $controller;
+
+    /**
+     * @var Container
+     */
+    protected $container;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->container = Container::getInstance();
+        $this->controller = $this->container->make(ServersController::class);
+    }
+
+    public function testIndex()
+    {
+        $response = $this->controller->index();
+        $this->assertInstanceOf(\Illuminate\View\View::class, $response);
+    }
+
+    public function testCreate()
+    {
+        $response = $this->controller->create();
+        $this->assertInstanceOf(\Illuminate\View\View::class, $response);
+    }
+
+    public function testEdit()
+    {
+        $server = new Server();
+
+        $response = $this->controller->edit($server);
+        $this->assertInstanceOf(\Illuminate\View\View::class, $response);
+    }
+
+    public function testStore()
+    {
+        $this->container = Container::getInstance();
+        $this->controller = $this->container->make(ServersController::class);
+
+        $request = ServerCreateRequest::create('/admin/server', ServerCreateRequest::METHOD_POST, [
+            'enabled' => 1,
+            'blocked' => 0,
+            'installed' => 1,
+            'name' => 'Test Server',
+            'game_id' => 'minecraft',
+            'ds_id' => 1,
+            'game_mod_id' => 8,
+            'server_ip' => 'localhost',
+            'server_port' => 27500,
+            'query_port' => 27500,
+            'rcon_port' => 27501,
+        ]);
+
+        $response = $this->controller->store($request);
+        $this->assertEquals(Response::HTTP_FOUND, $response->getStatusCode());
+    }
+
+    public function testUpdate()
+    {
+        $server = Server::get()->first();
+
+        $request = ServerUpdateRequest::create('/admin/server/', ServerCreateRequest::METHOD_PATCH, [
+            'name' => 'Edited Server',
+            'dir' => 'Dir',
+        ]);
+
+        $response = $this->controller->update($request, $server);
+        $this->assertEquals(Response::HTTP_FOUND, $response->getStatusCode());
+    }
+
+    public function testDestroy()
+    {
+        $server = Server::get()->first();
+
+        $response = $this->controller->destroy($server);
+        $this->assertEquals(Response::HTTP_FOUND, $response->getStatusCode());
+    }
+}
