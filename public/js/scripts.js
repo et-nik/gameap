a144e6b03bdb9c53e7c1a167dbd0836ac68219c7
Sergey Abu <dev@iteelion.ru>
2019-07-16 20:23:12 +0300

    UI: Upgrade left menu

---
+++ public/js/scripts.js
@@ -0,0 +1,7 @@
+$(function() {
+
+    $(document).on('click', '#left-menu-btn', function(){
+        $("#left-menu").toggle('display');
+    })
+
+});
--- resources/lang/en/sidebar.php
+++ resources/lang/en/sidebar.php
@@ -12,7 +12,24 @@ return [
 
     'profile'           => 'Profile',
     'view_profile'      => 'View profile',
-    'support'           => 'Support',
+    'logout'            => 'Logout',
 
-    'servers'      => 'Servers',
+    'admin'             => 'Administration',
+    'dedicated_servers' => 'Dedicated server',
+    'game_servers'      => 'Game servers',
+    'games'             => 'Games',
+    'gdaemon_tasks'     => 'GDaemon tasks',
+    'users'             => 'Users',
+
+    'control'           => 'Control',
+    'servers'           => 'Servers',
+
+    'gameap'            => 'Gameap',
+    'modules'           => 'Modules',
+    'update'            => 'Update',
+
+    'support'           => 'Support',
+    'documentation'     => 'Documentation',
+    'help'              => 'Help',
+    'report_bug'        => 'Report bug',
 ];
