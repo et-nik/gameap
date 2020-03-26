913743f4b0472e4b893c90ffea6e3b361abcb33c
et-nik <nikita.hldm@gmail.com>
2020-03-26 12:08:24 +0300

    Fix settings blade. Empty gameMod vars checking

--- resources/views/servers/settings.blade.php
+++ resources/views/servers/settings.blade.php
@@ -18,31 +18,34 @@
             <div class="card">
                 <div class="card-body">
                     {!! Form::model($server, ['method' => 'PATCH', 'route' => ['servers.updateSettings', $server->id], 'id' => 'adminServerForm']) !!}
-                    @foreach ($server->gameMod->vars as $var)
-                        @if (isset($var['admin_var']) && $var['admin_var'])
-                            @cannot('admin roles & permissions')
-                                @continue
-                            @endcannot
+
+                        @if(empty(!$server->gameMod->vars))
+                            @foreach ($server->gameMod->vars as $var)
+                                @if (isset($var['admin_var']) && $var['admin_var'])
+                                    @cannot('admin roles & permissions')
+                                        @continue
+                                    @endcannot
+                                @endif
+
+                                <div class="col-12">
+                                    {{ Form::bsText(
+                                            'vars[' . $var['var'] . ']',
+                                            (isset($server->vars[ $var['var'] ]))
+                                                ? $server->vars[ $var['var'] ]
+                                                : $var['default'],
+                                            $var['info']
+                                        ) }}
+                                </div>
+                            @endforeach
                         @endif
 
-                        <div class="col-12">
-                            {{ Form::bsText(
-                                    'vars[' . $var['var'] . ']',
-                                    (isset($server->vars[ $var['var'] ]))
-                                        ? $server->vars[ $var['var'] ]
-                                        : $var['default'],
-                                    $var['info']
-                                ) }}
-                        </div>
-                    @endforeach
-
-                    <div class="row mt-2">
-                        <div class="col-12">
-                            <div class="form-group">
-                                {{ Form::submit(__('main.save'), ['class' => 'btn btn-success']) }}
+                        <div class="row mt-2">
+                            <div class="col-12">
+                                <div class="form-group">
+                                    {{ Form::submit(__('main.save'), ['class' => 'btn btn-success']) }}
+                                </div>
                             </div>
                         </div>
-                    </div>
 
                     {!! Form::close() !!}
                 </div>
