b9f0bd17a7ac4b62b084051482ccf847caec14da
et-nik <nikita.hldm@gmail.com>
2021-12-12 02:17:16 +0300

    update version

--- config/constants.php
+++ config/constants.php
@@ -2,6 +2,6 @@
 //file : app/config/constants.php
 
 return [
-    'AP_VERSION' => '3.0.0-beta6',
-    'AP_DATE'    => '20.11.2021'
+    'AP_VERSION' => '3.0.0-beta7',
+    'AP_DATE'    => '12.12.2021'
 ];
