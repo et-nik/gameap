2163ff10787ecc4eb975ce5bd28d01b614125d71
et-nik <nikita.hldm@gmail.com>
2022-06-18 20:21:28 +0200

    update game mod context trait

--- tests/Context/Browser/Models/GameModContextTrait.php
+++ tests/Context/Browser/Models/GameModContextTrait.php
@@ -9,8 +9,9 @@ trait GameModContextTrait
     private function givenGameMod(): GameMod
     {
         return factory(GameMod::class)->create([
-            'game_code'         => 'test',
-            'name'              => 'Test Game Mod',
+            'game_code'               => 'test',
+            'name'                    => 'Test Game Mod',
+            'remote_repository_linux' => '',
         ]);
     }
 }
