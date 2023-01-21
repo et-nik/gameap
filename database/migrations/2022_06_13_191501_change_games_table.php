bc3b6695e0343f3cf313ca8aa94ccd5dea6900c5
et-nik <nikita.hldm@gmail.com>
2023-01-21 12:49:27 +0100

    fix migration

--- database/migrations/2022_06_13_191501_change_games_table.php
+++ database/migrations/2022_06_13_191501_change_games_table.php
@@ -15,9 +15,14 @@ class ChangeGamesTable extends Migration
     {
         Schema::table('games', function (Blueprint $table) {
             $table->renameColumn('steam_app_id', 'steam_app_id_linux');
+        });
+        Schema::table('games', function (Blueprint $table) {
             $table->renameColumn('remote_repository', 'remote_repository_linux');
+        });
+        Schema::table('games', function (Blueprint $table) {
             $table->renameColumn('local_repository', 'local_repository_linux');
         });
+
         Schema::table('games', function (Blueprint $table) {
             $table->integer('steam_app_id_windows')->after('steam_app_id_linux')->unsigned()->nullable();
             $table->string('remote_repository_windows')->after('remote_repository_linux')->nullable();
@@ -34,11 +39,15 @@ class ChangeGamesTable extends Migration
     {
         Schema::table('games', function (Blueprint $table) {
             $table->renameColumn('steam_app_id_linux', 'steam_app_id');
+        });
+        Schema::table('games', function (Blueprint $table) {
             $table->renameColumn('remote_repository_linux', 'remote_repository');
+        });
+        Schema::table('games', function (Blueprint $table) {
             $table->renameColumn('local_repository_linux', 'local_repository');
-            $table->dropColumn('steam_app_id_windows');
-            $table->dropColumn('remote_repository_windows');
-            $table->dropColumn('local_repository_windows');
+        });
+        Schema::table('games', function (Blueprint $table) {
+            $table->dropColumn(['steam_app_id_windows', 'remote_repository_windows', 'local_repository_windows']);
         });
     }
 }
