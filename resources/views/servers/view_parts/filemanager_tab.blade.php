63526a8b2f8ce54ab7be92b7b592eb5f7c86e284
et-nik <nikita.hldm@gmail.com>
2023-05-01 16:48:05 +0200

    fix file-manager baseUrl

--- resources/views/servers/view_parts/filemanager_tab.blade.php
+++ resources/views/servers/view_parts/filemanager_tab.blade.php
@@ -4,7 +4,7 @@
             <file-manager
                     v-if="activeTab === 'filemanager'"
                     :settings="{{ json_encode([
-                    'baseUrl' => url('file-manager/'.$server->id),
+                    'baseUrl' => url('file-manager/'.$server->id).'/',
                     'headers' => [
                         'X-Requested-With' => 'XMLHttpRequest',
                     ],
