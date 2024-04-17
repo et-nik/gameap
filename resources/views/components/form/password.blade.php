5eb4378acd46aedf8df5b9487a110eaf044a440c
et-nik <nikita.hldm@gmail.com>
2024-04-17 18:07:58 +0200

    vue update to tailwindcss

--- resources/views/components/form/password.blade.php
+++ resources/views/components/form/password.blade.php
@@ -7,8 +7,8 @@
 @endif
 
 <div class="mb-3{{ $errors->has($name) ? ' has-error' : '' }}">
-    {{ Form::label($name, $label, ['class' => 'control-label']) }}
-    {{ Form::password($name, array_merge(['class' => 'form-control'], $attributes)) }}
+    {{ Form::label($name, $label, ['class' => 'block mb-2 text-sm font-medium text-gray-900 dark:text-white']) }}
+    {{ Form::password($name, array_merge(['class' => 'block appearance-none w-full py-1 px-2 mb-1 text-base leading-normal bg-white text-gray-800 border border-gray-200 rounded'], $attributes)) }}
 
     @if ($errors->has($name))
         <span class="help-block">
--- resources/views/components/form/text.blade.php
+++ resources/views/components/form/text.blade.php
@@ -11,8 +11,8 @@
 @endif
 
 <div class="mb-3{{ $errors->has($name) ? ' has-error' : '' }}">
-    {{ Form::label($name, $label, ['class' => 'control-label']) }}
-    {{ Form::text($name, $value, array_merge(['class' => 'form-control'], $attributes)) }}
+    {{ Form::label($name, $label, ['class' => 'block mb-2 text-sm font-medium text-gray-900 dark:text-white']) }}
+    {{ Form::text($name, $value, array_merge(['class' => 'block appearance-none w-full py-1 px-2 mb-1 text-base leading-normal bg-white text-gray-800 border border-gray-200 rounded'], $attributes)) }}
 
     @if(isset($description))
         <small>{!! $description !!}</small>
