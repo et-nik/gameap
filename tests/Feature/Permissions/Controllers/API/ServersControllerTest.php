52b41caf700597aca5c0ba09aa037157ac1a034a
et-nik <nikita.hldm@gmail.com>
2024-05-26 18:37:17 +0200

    trying to fix permission tests

--- tests/Feature/Permissions/Controllers/API/ServersControllerTest.php
+++ tests/Feature/Permissions/Controllers/API/ServersControllerTest.php
@@ -94,7 +94,9 @@ class ServersControllerTest extends PermissionsTestCase
 
             /** @var TestResponse $response */
             $response = $this->{$method}(route($route, $params), []);
-            $response->assertStatus(Response::HTTP_FORBIDDEN);
+            if ($response->status() !== Response::HTTP_FORBIDDEN) {
+            	$this->fail('Response status is not 403, for route'. $route . ' with params: ' . json_encode($params));
+            }
         }
     }
 }
